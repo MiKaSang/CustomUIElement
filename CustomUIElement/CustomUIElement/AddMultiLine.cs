30401496482d1d0d1bece2b2c7e3c90b547052d8
Your Name <you@example.com>
2016-06-16 17:23:22 +0800

    Codes

---
+++ CustomUIElement/CustomUIElement/AddMultiLine.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Display;
+
+namespace CustomUIElement
+{
+    public class AddMultiLine : ESRI.ArcGIS.Desktop.AddIns.Tool
+    {
+        public AddMultiLine()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+
+        protected override void OnMouseDown(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
+        {
+            //Get the active view from the ArcMap static class.
+            IActiveView activeView = ArcMap.Document.ActiveView;
+            //If it's a polyline object, get from the user's mouse clicks.
+            IPolyline polyline = GetPolylineFromMouseClicks(activeView);
+            //Make a color to draw the polyline.
+            IRgbColor rgbColor = new RgbColorClass();
+            rgbColor.Red = 255;
+            //Add the user's drawn graphics as persistent on the map.
+            AddGraphicToMap(activeView.FocusMap, polyline, rgbColor, rgbColor);
+            //Best practice: Redraw only the portion of the active view that contains graphics.
+            activeView.PartialRefresh(esriViewDrawPhase.esriViewGraphics, null, null);
+        }
+
+        public void AddGraphicToMap(IMap map, IGeometry geometry, IRgbColor rgbColor, IRgbColor outlineRgbColor)
+        {
+            IGraphicsContainer graphicsContainer = (IGraphicsContainer)map; // Explicit Cast
+            IElement element = null;
+            if ((geometry.GeometryType) == esriGeometryType.esriGeometryPoint)
+            {
+                // Marker symbols
+                ISimpleMarkerSymbol simpleMarkerSymbol = new SimpleMarkerSymbolClass();
+                simpleMarkerSymbol.Color = rgbColor;
+                simpleMarkerSymbol.Outline = true;
+                simpleMarkerSymbol.OutlineColor = outlineRgbColor;
+                simpleMarkerSymbol.Size = 15;
+                simpleMarkerSymbol.Style = esriSimpleMarkerStyle.esriSMSCircle;
+                IMarkerElement markerElement = new MarkerElementClass();
+                markerElement.Symbol = simpleMarkerSymbol;
+                element = (IElement)markerElement; // Explicit Cast
+            }
+            else if ((geometry.GeometryType) == esriGeometryType.esriGeometryPolyline)
+            {
+                // Line elements
+                ISimpleLineSymbol simpleLineSymbol = new SimpleLineSymbolClass();
+                simpleLineSymbol.Color = rgbColor;
+                simpleLineSymbol.Style = esriSimpleLineStyle.esriSLSSolid;
+                simpleLineSymbol.Width = 5;
+                ILineElement lineElement = new LineElementClass();
+                lineElement.Symbol = simpleLineSymbol;
+                element = (IElement)lineElement; // Explicit Cast
+            }
+            else if ((geometry.GeometryType) == esriGeometryType.esriGeometryPolygon)
+            {
+                // Polygon elements
+                ISimpleFillSymbol simpleFillSymbol = new SimpleFillSymbolClass();
+                simpleFillSymbol.Color = rgbColor;
+                simpleFillSymbol.Style = esriSimpleFillStyle.esriSFSForwardDiagonal;
+                IFillShapeElement fillShapeElement = new PolygonElementClass();
+                fillShapeElement.Symbol = simpleFillSymbol;
+                element = (IElement)fillShapeElement; // Explicit Cast
+            }
+            if (!(element == null))
+            {
+                element.Geometry = geometry;
+                graphicsContainer.AddElement(element, 0);
+            }
+        }
+        public IPolyline GetPolylineFromMouseClicks(IActiveView activeView)
+        {
+            IScreenDisplay screenDisplay = activeView.ScreenDisplay;
+            IRubberBand rubberBand = new RubberLineClass();
+            IGeometry geometry = rubberBand.TrackNew(screenDisplay, null);
+            IPolyline polyline = (IPolyline)geometry;
+            return polyline;
+        }
+    }
+
+}
---
+++ CustomUIElement/CustomUIElement/ZoomToLayer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+
+namespace CustomUIElement
+{
+    public class ZoomToLayer : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ZoomToLayer()
+        {
+        }
+
+
+        private void ZoomToActiveLayerInTOC(IMxDocument mxDocument)
+        {
+            if (mxDocument == null)
+            {
+                return;
+            }
+            ESRI.ArcGIS.Carto.IActiveView activeView = mxDocument.ActiveView;
+            // Get the TOC
+            ESRI.ArcGIS.ArcMapUI.IContentsView IContentsView = mxDocument.CurrentContentsView;
+            // Get the selected layer
+            System.Object selectedItem = IContentsView.SelectedItem;
+            if (!(selectedItem is ESRI.ArcGIS.Carto.ILayer))
+            {
+                return;
+            }
+            ESRI.ArcGIS.Carto.ILayer layer = selectedItem as ESRI.ArcGIS.Carto.ILayer;
+            // Zoom to the extent of the layer and refresh the map
+            activeView.Extent = layer.AreaOfInterest;
+            activeView.Refresh();
+        }
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ZoomToActiveLayerInTOC(ArcMap.Application.Document as IMxDocument);
+            ArcMap.Application.CurrentTool = null;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
